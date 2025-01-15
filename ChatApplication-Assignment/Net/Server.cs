d4c475c3914a15af03b06b8d6600813bb602e18d
Amaliebra <jba082024@gmail.com>
2025-01-15 12:21:12 +0100

    folder structure complete

---
+++ ChatApplication-Assignment/Net/Server.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApplication_Assignment.Net
+{
+    internal class Server
+    {
+
+    }
+}
