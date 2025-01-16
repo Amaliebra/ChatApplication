efba60342376adedc0b8a66e3105f6c6027e5984
Amaliebra <amaliebratland@gmail.com>
2025-01-16 10:30:42 +0100

    added some of the logic to program, and duplicated packetbuilder to clientside

--- ChatApplication-Assignment/Net/IO/PacketBuilder.cs
+++ ChatApplication-Assignment/Net/IO/PacketBuilder.cs
@@ -1,12 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ChatClient.Net.IO
 {
-    internal class PacketBuilder
+    public class PacketBuilder
     {
+        private MemoryStream _packetStream;
+
+        public PacketBuilder()
+        {
+            _packetStream = new MemoryStream();
+        }
+
+        public void WriteOpCode(byte opCode)
+        {
+            _packetStream.WriteByte(opCode);
+        }
+
+        public void WriteString(string message)
+        {
+            var length = Encoding.UTF8.GetByteCount(message);
+            _packetStream.Write(BitConverter.GetBytes(length), 0, 4);
+            _packetStream.Write(Encoding.UTF8.GetBytes(message), 0, length);
+        }
+
+        public byte[] GetPacketBytes()
+        {
+            return _packetStream.ToArray();
+        }
+
+        public void Reset()
+        {
+            _packetStream = new MemoryStream();
+        }
     }
 }
--- ChatServer/Program.cs
+++ ChatServer/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System.Net;
+using System.Net.Sockets;
+
+public class ChatServerProgram
+{
+    private TcpListener _server;
+    private List<TcpClient> _clients;
+
+    public ChatServerProgram(string ip, int port)
+    {
+        _server = new TcpListener(IPAddress.Any, port);
+        _clients = new List<TcpClient>();
+    }
+
+    public async Task StartAsync()
+    {
+        _server.Start();
+        Console.WriteLine("Server started");
+        while (true)
+        {
+            var client = await _server.AcceptTcpClientAsync();
+            _clients.Add(client);
+            Console.WriteLine("Client connected");
+            Task.Run(() => HandleClientAsync(client));
+        }
+    }
+
+    private async Task HandleClientAsync(TcpClient client)
+    {
+        throw new NotImplementedException();
+    }
+}
