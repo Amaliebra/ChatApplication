3f8a2ced20ef030f4907107b337f4292a5452172
Amaliebra <amaliebratland@gmail.com>
2025-02-12 12:41:19 +0100

    added converters, now trying to get converters to work

---
+++ ChatApplication-Assignment/Converters/BoolToAlignment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Data;
+
+namespace ChatClient.Converters
+{
+    public class BoolToAlignment : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is bool boolValue)
+            {
+                return boolValue ? HorizontalAlignment.Right : HorizontalAlignment.Left;
+            }
+            return HorizontalAlignment.Left; 
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException("");
+        }
+    }
+}
