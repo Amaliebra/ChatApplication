e33b7e2a54b9cab4309b03201da42b7bd9634cd5
Amaliebra <amaliebratland@gmail.com>
2025-02-12 13:08:56 +0100

    Chatbubbles work finally

--- ChatApplication-Assignment/Converters/BoolToAlignment.cs
+++ ChatApplication-Assignment/Converters/BoolToAlignment.cs
@@ -6,13 +6,15 @@ namespace ChatClient.Converters
 {
     public class BoolToAlignment : IValueConverter
     {
+        public object TrueValue { get; set; }
+        public object FalseValue { get; set; }
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
             if (value is bool boolValue)
             {
-                return boolValue ? HorizontalAlignment.Right : HorizontalAlignment.Left;
+                return boolValue ? TrueValue : FalseValue;
             }
-            return HorizontalAlignment.Left; 
+            return HorizontalAlignment.Left;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
